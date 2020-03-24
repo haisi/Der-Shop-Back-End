a4ad343904d4567c8a76fdc507a681cf528190ed
Hasan Kara <hasan.selman.kara@gmail.com>
2020-03-24 21:16:45 +0100

    Dynamic link resource url

--- src/main/java/li/selman/dershop/product/ProductResourceProcessor.java
+++ src/main/java/li/selman/dershop/product/ProductResourceProcessor.java
@@ -1,9 +1,11 @@
 package li.selman.dershop.product;
 
-import org.springframework.hateoas.EntityModel;
-import org.springframework.hateoas.Link;
+import org.springframework.hateoas.*;
 import org.springframework.hateoas.server.RepresentationModelProcessor;
 import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Hasan Selman Kara
@@ -15,7 +17,18 @@ public class ProductResourceProcessor implements RepresentationModelProcessor<En
 
     @Override
     public EntityModel<Product> process(EntityModel<Product> resource) {
-        resource.add(Link.of("http:www.s3.amazon.com/product-image/1", IMAGE_REL));
+        if (resource.getContent() == null) {
+            return resource;
+        }
+
+        resource.add(Link.of(geProductImageUrl(resource.getContent()), IMAGE_REL));
         return resource;
+    }
+
+    String geProductImageUrl(Product product) {
+        UriTemplate template = UriTemplate.of("http:www.s3.amazon.com/product-image/{id}");
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("id", product.getId().toString());
+        return template.expand(uriVariables).toString();
     }
 }
