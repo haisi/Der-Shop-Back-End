8b6f0528849ab5a2598e8c53d21521495b7a0da3
Hasan Kara <hasan.selman.kara@gmail.com>
2021-05-06 21:20:21 +0200

    Add Spring security

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,14 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-hateoas")
 
+    // Security
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("io.jsonwebtoken:jjwt-api:0.11.2")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.2")
+    // Uncomment the next line if you want to use RSASSA-PSS (PS256, PS384, PS512) algorithm
+//    runtimeOnly("org.bouncycastle:bcprov-jdk15on:1.60")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.2")
+
     // Monitoring
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("io.micrometer:micrometer-registry-prometheus")
