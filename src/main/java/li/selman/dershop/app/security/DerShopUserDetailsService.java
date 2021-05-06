22bbc59be0c0aa7512df0b5c353f70364c57f385
Hasan Kara <hasan.selman.kara@gmail.com>
2021-05-06 23:57:20 +0200

    Implement UserDetailsService

---
+++ src/main/java/li/selman/dershop/app/security/DerShopUserDetailsService.java
@@ -0,0 +1,82 @@
+/*
+ * (c) Copyright 2021 Hasan Selman Kara. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package li.selman.dershop.app.security;
+
+import java.text.MessageFormat;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+import li.selman.dershop.user.DerShopUser;
+import li.selman.dershop.user.UserRepository;
+import org.hibernate.validator.internal.constraintvalidators.bv.EmailValidator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+public class DerShopUserDetailsService implements UserDetailsService {
+
+    private static final Logger log = LoggerFactory.getLogger(DerShopUserDetailsService.class);
+
+    private final UserRepository userRepository;
+
+    public DerShopUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(final String login) {
+        log.debug("Authenticating {}", login);
+
+        if (new EmailValidator().isValid(login, null)) {
+            return userRepository
+                .findOneWithAuthoritiesByEmailIgnoreCase(login)
+                .map(derShopUser -> createSpringSecurityUser(login, derShopUser))
+                .orElseThrow(() -> new UsernameNotFoundException(
+                    MessageFormat.format("User with email {0} was not found in the database", login))
+                );
+        }
+
+        String lowercaseLogin = login.toLowerCase(Locale.ENGLISH);
+        return userRepository
+            .findOneWithAuthoritiesByLogin(lowercaseLogin)
+            .map(derShopUser -> createSpringSecurityUser(lowercaseLogin, derShopUser))
+            .orElseThrow(() -> new UsernameNotFoundException(
+                MessageFormat.format("User {0} was not found in the database", lowercaseLogin))
+            );
+    }
+
+    private User createSpringSecurityUser(String lowercaseLogin, DerShopUser derShopUser) {
+        if (!derShopUser.isActivated()) {
+            throw new UserNotActivatedException("User " + lowercaseLogin + " was not activated");
+        }
+        List<GrantedAuthority> grantedAuthorities = derShopUser
+            .getAuthorities()
+            .stream()
+            .map(authority -> new SimpleGrantedAuthority(authority.getName()))
+            .collect(Collectors.toList());
+        return new User(derShopUser.getLogin(), derShopUser.getPassword(), grantedAuthorities);
+    }
+}
