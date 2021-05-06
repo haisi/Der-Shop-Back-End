b46cb369538a8ca71882528e0bc538f8f7197483
Hasan Kara <hasan.selman.kara@gmail.com>
2021-05-07 00:33:54 +0200

    Add temporary data initialization

--- src/main/java/li/selman/dershop/DerShopApplication.java
+++ src/main/java/li/selman/dershop/DerShopApplication.java
@@ -15,16 +15,25 @@
  */
 package li.selman.dershop;
 
+import li.selman.dershop.app.security.jwt.JwtProperties;
 import li.selman.dershop.product.Product;
 import li.selman.dershop.product.ProductRepository;
+import li.selman.dershop.user.Authority;
+import li.selman.dershop.user.AuthorityRepository;
+import li.selman.dershop.user.DerShopUser;
+import li.selman.dershop.user.UserRepository;
+import li.selman.dershop.user.UserService;
+import li.selman.dershop.user.dto.AdminUserDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(JwtProperties.class)
 public class DerShopApplication {
 
     private static final Logger log = LoggerFactory.getLogger(DerShopApplication.class);
@@ -34,11 +43,26 @@ public class DerShopApplication {
     }
 
     @Bean
-    public CommandLineRunner init(ProductRepository repo) {
+    public CommandLineRunner init(ProductRepository repo, UserRepository userRepo,
+                                  UserService userService, AuthorityRepository authRepo) {
         return args -> {
             repo.save(new Product("Banana"));
             repo.save(new Product("Apple"));
             repo.save(new Product("Kiwi"));
+
+            // TODO(#refactor): Load as part of Liquibase
+            Authority admin = new Authority();
+            admin.setName("admin");
+            admin = authRepo.save(admin);
+
+            AdminUserDto newUser = new AdminUserDto();
+            newUser.setLogin("admin@example.com");
+            newUser.setEmail("admin@example.com");
+            newUser.setCreatedBy("admin");
+            DerShopUser nU = userService.registerUser(newUser, "Hunter1");
+            nU.getAuthorities().add(admin);
+            nU.setActivated(true);
+            userRepo.save(nU);
 
             repo.findAll()
                 .forEach(it -> log.info(it.toString()));
