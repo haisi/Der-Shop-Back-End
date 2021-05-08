b3b3847b8293bc064a97a2e152b26ba3d3ad6627
Hasan Kara <hasan.selman.kara@gmail.com>
2021-05-08 14:49:09 +0200

    Add diagram support to asciidoc

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.springframework.boot.gradle.tasks.bundling.BootJar
 plugins {
     id("org.springframework.boot") version "2.4.5"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    id("org.asciidoctor.convert") version "1.5.8"
+    id("org.asciidoctor.jvm.convert") version "3.1.0"
 	id("java")
     jacoco
     id("com.palantir.baseline") version "3.69.0"
@@ -17,8 +17,6 @@ java.sourceCompatibility = JavaVersion.VERSION_11
 repositories {
     mavenCentral()
 }
-
-val snippetsDir by extra { file("build/generated-snippets") }
 
 dependencies {
     // Database Access
@@ -61,26 +59,7 @@ dependencies {
     testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
 }
 
-tasks.withType<Test> {
-    useJUnitPlatform()
-}
-
-tasks.test {
-    outputs.dir(snippetsDir)
-}
-
-// Disable error prone for test-sources
-tasks.named<JavaCompile>("compileTestJava") {
-    options.errorprone.isEnabled.set(false)
-}
-
-tasks.withType<JavaCompile>().configureEach {
-    options.errorprone.disable("BracesRequired", "MissingSummary", "EqualsGetClass", "OptionalOrElseMethodInvocation",
-        "PreferSafeLoggableExceptions", "PreferSafeLoggingPreconditions", "Slf4jConstantLogMessage",
-        "SameNameButDifferent", // Due to Lombok
-        "StrictUnusedVariable" // Re-enable in the future
-    )
-}
+val snippetsDir by extra { file("build/generated-snippets") }
 
 spotless {
     java {
@@ -101,6 +80,34 @@ spotless {
 java {
     withSourcesJar()
     withJavadocJar()
+}
+
+asciidoctorj {
+    modules {
+        diagram.use()
+        diagram.setVersion("2.0.5")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+tasks.test {
+    outputs.dir(snippetsDir)
+}
+
+// Disable error prone for test-sources
+tasks.named<JavaCompile>("compileTestJava") {
+    options.errorprone.isEnabled.set(false)
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.errorprone.disable("BracesRequired", "MissingSummary", "EqualsGetClass", "OptionalOrElseMethodInvocation",
+        "PreferSafeLoggableExceptions", "PreferSafeLoggingPreconditions", "Slf4jConstantLogMessage",
+        "SameNameButDifferent", // Due to Lombok
+        "StrictUnusedVariable" // Re-enable in the future
+    )
 }
 
 tasks.withType<Javadoc>().configureEach {
